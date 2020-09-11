d8436d1eb30e15b78fac122a5a3051cdec9dbc0d
CooperDoten <cooper.doten@gmail.com>
2020-09-11 18:41:42 -0500

    successful POST route created client to server to DB

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,38 @@
 $(document).ready(onReady);
 
-
 function onReady() {
     console.log(`JQ sourced and in client.js`);
+    //call function to load tasks
+    //getTasks()
+    //on click event call makeTask
+    $(document).on('click', '#submitBtn', makeTask);
 }
+
+function makeTask() {
+    //create object to send to POST
+    let task = {
+        name: $('#nameIn').val(),
+        age: $('#ageIn').val(),
+        task: $('#taskIn').val(),
+        type: $('#typeIn').val(),
+    }
+    //call function to send object to POST
+    taskToPost(task);
+}
+
+function taskToPost(task) {
+    $.ajax({
+        type: 'POST',
+        url: '/tasks',
+        data: task
+    }).then(function (response) {
+        console.log('response came back with', response);
+       // getTasks();
+    }).catch(function (error) {
+        console.log('error in POST', error);
+    })
+}
+
+
+
+
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -5,11 +5,27 @@ const pg = require('pg');
 const Pool = pg.Pool;
 
 const pool = new Pool({
-  database: "tasks",
+  database: "task",
   host: "localhost",
   port: 5432,
   max: 12,
   idleTimeoutMillis: 20000
+});
+
+router.post('/', (req, res) => {
+    let task = req.body;
+    console.log(`Adding task`, task);
+
+    let queryText = `INSERT INTO "tasks" ("name", "age", "task", "type")
+    VALUES ($1, $2, $3, $4);`;
+    pool.query(queryText, [task.name, task.age, task.task, task.type])
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.log(`Error adding new task`, error);
+            res.sendStatus(500);
+        });
 });
 
 module.exports = router;
